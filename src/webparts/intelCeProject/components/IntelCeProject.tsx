be5b9cc871377149875825ca430798162e7ea759
daekeun-kim <daekeun.kim@kla-tencor.com>
2018-09-10 22:28:13 +0900

    test4

--- src/webparts/intelCeProject/components/IntelCeProject.tsx
+++ src/webparts/intelCeProject/components/IntelCeProject.tsx
@@ -12,6 +12,7 @@ export default class IntelCeProject extends React.Component<IIntelCeProjectProps
             <div className={ styles.column }>
               <span className={ styles.title }>Welcome to SharePoint!</span>
               <span className={ styles.title }>Welcome to SharePoint!</span>
+              <span className={ styles.title }>Welcome to SharePoint!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
